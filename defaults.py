609af36127e79133c4643c9b33c135ae720048c5
Rui Fernandes <ruipedro16@protonmail.com>
2025-12-09 22:27:36 +0000

    fix

--- defaults.py
+++ defaults.py
@@ -9,4 +9,4 @@ DEFAULT_DECRYPTED_VIDEO_FILENAME: str = "OK_video.mp4"
 DEFAULT_MERGED_VIDEO_FILENAME: str = "Ficheiro_Final.mp4"
 VIDEO_EXTENSIONS: list[str] = [".mp4", ".mkv"]  # TODO: add more if needed
 
-KEYS_URL: str = "https://cdrm-project.com"
+KEYS_URL: str = "https://cdrm-project.com/"
